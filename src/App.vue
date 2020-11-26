5694d03a4c59f54dd3a72c8a9801f082971bff57
Shiohne <Shiohne@github.com>
2020-11-26 01:33:03 -0500

    Prueba nuevo landing

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
-  <div id="app">
-    <!-- Navigation Bar -->
-     <div id="bootstrap-overrides">
-      <!-- Navigation Bar -->
-      <nav class="navbar mynav">
-        <div class="col-lg-2.1 shadow" style="cursor: pointer;" onclick="window.location='/';">
-          <h4 class="differ1">Minimarket</h4>
+  <div>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Landing</title>
+    <link rel="stylesheet" href="estilos.css">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
+  <div>
+  <header>
+    <div class="container">
+      <nav>
+        <a href="#" id="icono" class="icono">Menú</a>
+        <div class="enlaces uno" id="enlaces">
+          <a href="">Inicio</a>
+          <a href="">Acerca de</a>
+          <a href="">Contacto</a>
         </div>
-          <ul class="nav-links">
-             <li>
-                <a href="/terms&conditions">Terminos y condiciones</a>
-             </li>
-          </ul>
-          <div class="burger" @click="navSlide">
-              <div class="line1"></div>
-              <div class="line2"></div>
-              <div class="line3"></div>
-          </div>
-      </nav> 
-
-    <section class="zoom">
-        <p>-</p>
-        <!-- Router View -->
-        <div class="container mt-3">
-          <router-view/>
-        </div>
-      </section>
-     </div>
+      </nav>
+      <div class="textos">
+        <h1>MINIMARKET</h1>
+        <h2>Hacemos la web ideal para ti</h2>
+        <a href="#">Contacto</a>
+      </div>
+      <img src="src/assets/calculator.svg" alt="">
+    </div>
+  </header>
+  <div class="wave">
+    <div style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-9.93,-12.53 C175.16,239.10 337.69,-135.89 505.30,71.34 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: rgb(255, 255, 255);"></path></svg></div>
+  </div>
+  </div>
   </div>
 </template>
 
@@ -51,172 +53,129 @@ export default {
 
 <style scoped>
 *{
-    margin: 0px;
-    padding: 0px; 
-    box-sizing: border-box;
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-body {
-    background-color: white;
+body{
+
+  font-family: 'Raleway', sans-serif;
 }
 
-h1 {
-    color: white;
-    font-size: 100px;
-    font-family: 'Cabin', sans-serif;
+nav{
+  position: fixed;
+  top: 20px;
+  left: 20px;
+  display: flex;
+  justify-content: flex-start;
 }
 
-p {
-    color: black;
-    text-align: justify;
-    display: flex;
-    width: 90%;
-    letter-spacing: 2px;
-    font-family: 'Cabin', sans-serif;
-    padding: 15px;
-
-}
-/*Menú arriba*/
-#bootstrap-overrides nav {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    min-height: 8vh;
-    font-family: 'Cabin', sans-serif;
-  background-color: rgb(0, 102, 255, 0.0);
-    position: fixed;
-    top: 0;
-    width: 100%;
+nav a{
+  color:#fff;
+  text-decoration: none;
 }
 
-#bootstrap-overrides .navbar {
-    -webkit-box-shadow: 0 8px 6px -6px #999;
-    -moz-box-shadow: 0 8px 6px -6px #999;
-    box-shadow: 0 8px 6px -6px #999;
-    z-index: 1000;
+.icono{
+  display: block;
+  z-index: 100000000;
+  animation: moverIzquierda 1s ease-in;
 }
 
-#bootstrap-overrides .logo {
-    color: #a95a5a;
-    letter-spacing: 3px;
-    font-size: 26px;
+header{
+  background: #DA4453;
+  background: -webkit-linear-gradient(to right, #89216B, #DA4453);
+  background: linear-gradient(to right, #89216B, #DA4453);
+  height: auto;
+  padding: 48px;
+  width: 100%;
 }
 
-#bootstrap-overrides .nav-links {
-    display: flex;
-    justify-content: space-around;
-    width: 45%;
-    margin: 0px;
-    padding: 0px; 
-    box-sizing: border-box;
+.container{
+  width: 95%;
+  max-width: 1200px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin: auto;
 }
 
-#bootstrap-overrides .nav-links li {
-    list-style: none;
-    margin: 0px;
-    padding: 0px; 
-    box-sizing: border-box;
+img{
+  display: block;
+  height: 450px;
+  object-fit: cover;
+  animation: arriba 1s ease-in;
 }
 
-#bootstrap-overrides .nav-links a{
-    text-decoration: none;
-    color: #ffffff;
-    /* font-weight: bold; */
-    font-size: 18px;
-    margin: 0px;
-    padding: 0px; 
-    box-sizing: border-box;
+.textos{
+  width: 50%;
+  color:#fff;
 }
 
-#bootstrap-overrides .burger {
-    display: none;
-    cursor: pointer;
-}
-
-#bootstrap-overrides .burger div{
-    width: 20px;
-    height: 2px;
-    background-color: black;
-    margin: 5px;
-    transition: all 0.3s ease;
-}
-
-.differ1{
-  color: white;
-  letter-spacing: 2px;
-}
-
-.shadow{
-  margin-right: fill;
-}
-@media screen and (max-width:1024px){
-    #bootstrap-overrides .nav-links {
-       width: 50%;
-   }
+.textos h1{
+  font-size:80px;
+  animation: moverDerecha 1s ease-in;
 
 }
 
-    body {
-        overflow-x: hidden;
-    }
-
-    /*Menú desplegable*/
-    #bootstrap-overrides .nav-links{
-        position: absolute;
-        right: 0px;
-        height: 92vh;
-        top: 8vh;
-        background-color: #ca2323;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        width: 10%;
-        transform: translateX(100%);
-        transition: transform 0.3s ease-in;
-        z-index: 101;
-    }
-
-    #bootstrap-overrides .nav-links a {
-        color: black;
-        opacity: 100%;
-    }
-
-    #bootstrap-overrides .burger {
-        display: block;
-    }
-
-    .zoom #layer1 {
-        height: auto;
-    }
-
-    .zoom #layer2 {
-        height: auto;
-    }
-
-
-
-#bootstrap-overrides .nav-action {
-    transform: translateX(0%);
+.textos h2{
+  font-size:30px;
+  animation: moverIzquierda 1s ease-in;
 }
 
-.toggle .line1{
-    transform: rotate(-45deg) translate(-4px, 5px);
+.textos a{
+  display: inline-block;
+  color:#fff;
+  font-weight: 300;
+  text-decoration: none;
+  margin-top: 30px;
+  border: 1px solid #fff;
+  width: 150px;
+  border-radius: 3px;
+  text-align: center;
+  padding: 10px 0;
+  animation: arriba 1s ease-in;
+
 }
 
-.toggle .line2{
-    opacity: 0%;
+.wave{
+  height: 100px;
+  width: 100%;
+  background: #DA4453;
+  background: -webkit-linear-gradient(to right, #c42597, #DA4453);
+  background: linear-gradient(to right, #89216B, #DA4453);
 }
 
-.toggle .line3{
-    transform: rotate(45deg) translate(-5px, -6px);
+.enlaces{
+  position: fixed;
+  display: flex;
+  top: 0;
+  background: #000;
+  justify-content: space-around;
+  align-items: center;
+  height: 100vh;
+  right: 0;
+  flex-direction: column;
+  width: 100%;
+  transition: all 1s ease;
+  background: #bc4e9c;
+  background: -webkit-linear-gradient(to right, #be3261, #bc4e9c);
+  background: linear-gradient(to right, #f80759, #e6aa3b);
+
 }
 
-.zoom{
-    width: 100%;
-    height: 100%;
-    position: fixed;
-    background: url(assets/fondo2.jpg);
-    background-size: 1550px
+.uno{
+
+  -webkit-clip-path: circle(0% at 0 0);
+  clip-path: circle(0% at 0 0);
+
+}
+
+.dos{
+
+  -webkit-clip-path: circle(150% at 0 0);
+  clip-path: circle(150% at 0 0);
+
 }
 
 </style>
